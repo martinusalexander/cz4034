7f7e27028bf26e49e6caf38be3ce8131cef0dfb8
martinusalexander <martinus.alexander.looho@gmail.com>
2017-02-10 01:13:59 +0800

    Updated configuration for the application

--- cz4034/urls.py
+++ cz4034/urls.py
@@ -1,12 +1,19 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'cz4034.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+    # Default admin panel, provided by Django
+    # url(r'^admin/', include(admin.site.urls)),
+
+    # Customized admin panel
+    url(r'^admin/', include('admin_panel.urls')),
+
+    # User application
+    url(r'^', include('user_app.urls')),
+]
