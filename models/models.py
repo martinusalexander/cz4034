f96a18b52fd3599ac3ae67ed3af0db2c514a109e
Martinus Alexander <martinus.alexander.looho@gmail.com>
2017-02-16 16:13:02 +0800

    Updated models and database according to Twitter data structure

--- models/models.py
+++ models/models.py
@@ -6,10 +6,14 @@ from django.db import models
 
 class Documents(models.Model):
     """
-    List of documents (temporary only)
+    List of documents
     """
     def __str__(self):
         return self.name
-    name = models.CharField(max_length=200, unique=True)
-    content = models.TextField(max_length=1000)
-    date = models.DateTimeField()
+    id = models.BigIntegerField(name='id', primary_key=True)
+    name = models.CharField(max_length=200, default="")
+    screen_name = models.CharField(max_length=200, default="")
+    status = models.CharField(max_length=1000, default="")
+    location = models.CharField(max_length=200, null=True)
+    source = models.CharField(max_length=500, null=True)
+    created_at = models.DateTimeField()
