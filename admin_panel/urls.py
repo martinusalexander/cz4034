0c12d1fadc4cdf6168e276e4ba7d86a30cad7875
Martinus Alexander <martinus.alexander.looho@gmail.com>
2017-02-22 11:38:40 +0800

    Renamed some function name for maintainability

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -3,12 +3,17 @@ from . import views
 
 
 urlpatterns = [
+
+    # Admin landing page
     url('^$', views.main, name='main'),
-    url('^sign_in/$', views.start_user_sign_in, name='start_sign_in'),
-    url('^sign_in/submit/$', views.user_sign_in, name='sign_in'),
-    url('^register/$', views.start_create_account, name='start_update_account'),
-    url('^create_account/submit/$', views.create_account, name='create_account'),
-    url('^edit_account/$', views.start_update_account, name='start_update_account'),
-    url('^edit_account/submit/$', views.update_account, name='edit_account'),
-    url('^logout/$', views.sign_out, name='sign_out')
+
+    # Account management
+    url('^sign_in/$', views.user_sign_in, name='sign_in'),
+    url('^sign_in/submit/$', views.user_sign_in_submit, name='sign_in_submit'),
+    url('^register/$', views.create_account, name='create_account'),
+    url('^create_account/submit/$', views.create_account_submit, name='create_account_submit'),
+    url('^edit_account/$', views.update_account, name='update_account'),
+    url('^edit_account/submit/$', views.update_account_submit, name='edit_account_submit'),
+    url('^logout/$', views.sign_out, name='sign_out'),
+
 ]
