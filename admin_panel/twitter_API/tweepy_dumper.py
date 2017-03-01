1b595f822384ab5d5491140546f9a8bfa2c63988
Martinus Alexander <martinus.alexander.looho@gmail.com>
2017-03-01 20:27:46 +0800

    Added scraping functionality

--- admin_panel/twitter_API/tweepy_dumper.py
+++ admin_panel/twitter_API/tweepy_dumper.py
@@ -5,60 +5,51 @@ import tweepy #https://github.com/tweepy/tweepy
 import csv
 
 #Twitter API credentials
+
 consumer_key = "BGM0wTogXmt1yCleGIgbMRFbS"
 consumer_secret = "NP0CgXxQo4xz5BdFiH76v7a6dBQNlTTlszsE2SOTJl44Ijekcb"
 access_key = "292434485-KOEeAtHI8Mao57fYJpJp3S5NQf9niiDGK1nfGe59"
 access_secret = "hm2mmd539zjT1mGC5BqQgpbygK0nqSBxh6CE0ImozzHZl"
+"""
+consumer_key = 'PSX3TZVXzB0DBEwsVOAvkSzQ2'
+consumer_secret = 'GRlMa4ICbDDm0ChmXuM6KSADs7g0jQPufoODb2gYanI0hkM4cE'
+access_key = '166141524-9VeZNOXRmTSSwHhGXqfyZOoStTzOdrp0b8FRp8d0'
+access_secret = 'bmGAkF2JsitdYIF17Mqqe8qYxqU1IupyUCe8bRflgsX9A'
+"""
 
-
-def get_all_tweets(screen_name):
+def get_all_tweets(search_keyword):
 	#Twitter only allows access to a users most recent 3240 tweets with this method
 
 	#authorize twitter, initialize tweepy
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_key, access_secret)
-	api = tweepy.API(auth)
+	api = tweepy.API(auth, wait_on_rate_limit=True)
 
 	#initialize a list to hold all the tweepy Tweets
-	alltweets = []
+	all_tweets = []
 
-	#make initial request for most recent tweets (200 is the maximum allowed count)
-	new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+	new_tweets = tweepy.Cursor(api.search,
+					  q=search_keyword,
+					  count=10,
+					  result_type="recent",
+					  include_entities=True,
+					  lang="en").items()
 
-	#save most recent tweets
-	alltweets.extend(new_tweets)
+	# Save tweets if it is not in the list
+	for tweet in new_tweets:
+		if tweet not in all_tweets:
+			all_tweets.append(tweet)
 
-	#save the id of the oldest tweet less one
-	oldest = alltweets[-1].id - 1
+	final_tweet_data = []
+	for tweet in all_tweets:
+		tweet_data = {'id': tweet.id,
+					  'name': tweet.user.name,
+					  'screen_name': tweet.user.screen_name,
+					  'status': tweet.text,
+					  'location': tweet.user.location,
+					  'source': tweet.source,
+					  'created_at': tweet.created_at}
+		print(tweet_data)
+		final_tweet_data.append(tweet_data)
 
-	#keep grabbing tweets until there are no tweets left to grab
-	while len(new_tweets) > 0:
-		print "getting tweets before %s" % (oldest)
-
-		#all subsiquent requests use the max_id param to prevent duplicates
-		new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
-
-		#save most recent tweets
-		alltweets.extend(new_tweets)
-
-		#update the id of the oldest tweet less one
-		oldest = alltweets[-1].id - 1
-
-		print "...%s tweets downloaded so far" % (len(alltweets))
-
-	#transform the tweepy tweets into a 2D array that will populate the csv
-	outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
-
-	#write the csv
-    # todo: dump to sql
-	# with open('%s_tweets.csv' % screen_name, 'wb') as f:
-	# 	writer = csv.writer(f)
-	# 	writer.writerow(["id","created_at","text"])
-	# 	writer.writerows(outtweets)
-    #
-	# pass
-
-
-if __name__ == '__main__':
-	#pass in the username of the account you want to download
-	get_all_tweets("SLiYinggg")
+	return final_tweet_data
--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from . import views
 
-
 urlpatterns = [
 
     # Admin landing page
@@ -15,5 +14,12 @@ urlpatterns = [
     url('^edit_account/$', views.update_account, name='update_account'),
     url('^edit_account/submit/$', views.update_account_submit, name='edit_account_submit'),
     url('^logout/$', views.sign_out, name='sign_out'),
+
+    # Data management
+    url('^scrape/$', views.scrape_main, name='scrape_main'),
+    url('^scrape/submit/$', views.perform_scrape, name='perform_scrape'),
+    url('^scrape_history/$', views.scrape_history_index, name='scrape_history_index'),
+    url('^view_scraped_documents/$', views.scraped_documents_index, name='scraped_documents_index'),
+    url('^view_all_documents/$', views.documents_index, name='documents_index'),
 
 ]
