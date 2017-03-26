86eb52f64530871934a8e43d5bd138236ac0d9ad
Martinus Alexander <martinus.alexander.looho@gmail.com>
2017-03-26 21:35:07 +0800

    Removed SOLR

--- solr/stop_solr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Go to SOLR folder
-# cd solr/solr-6.4.1/example
-#Report to user
-echo "Stopping SOLR server."
-# java -jar start.jar
-./solr/solr-6.4.1/bin/solr stop -all -e cloud -noprompt
