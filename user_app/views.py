afe830cfc3d61eaf58d3c6e22c73e51bf9c4bbed
Thaw23 <94thaw@gmail.com>
2017-03-31 17:22:25 +0800

    updated UI
    left with paging

--- user_app/views.py
+++ user_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from haystack.query import SearchQuerySet
 
 from forms import *
@@ -19,4 +20,15 @@ def search_documents(request):
     print(keyword)
     documents = SearchQuerySet().autocomplete(content=keyword).highlight().models(Hotel_Review)
     # documents = Documents.objects.filter(status__icontains=keyword)
+    user_list = documents.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(documents, 10)
+    try:
+        users = paginator.page(page)
+    except PageNotAnInteger:
+        users = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
+
     return render(request, 'result.html', {'keyword':keyword, 'documents': documents})
