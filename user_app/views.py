b2c7132fcda817a8c5a8a3b4c23dcd85fa6d12bc
Martinus Alexander <martinus.alexander.looho@gmail.com>
2017-04-06 17:30:59 +0800

    Merged title, logo, and advanced search

--- user_app/views.py
+++ user_app/views.py
@@ -21,14 +21,6 @@ def about(request):
 def search_documents(request):
 
     start_time = datetime.datetime.now()
-    # Get pages
-    try:
-        page = request.POST.__getitem__('page')
-        page = int(page)
-    except KeyError:
-        page = 1
-    if page <= 0:
-        page = 1
 
     # Get original query entered by user
     search_keyword_original = request.POST.__getitem__('search_keyword')
@@ -41,6 +33,7 @@ def search_documents(request):
         else:
             need_spell_correction = True
     except KeyError:
+        print("Not found")
         need_spell_correction = True
 
     search_keyword = optimize(search_keyword_original, do_spell_correction=need_spell_correction)
@@ -58,6 +51,49 @@ def search_documents(request):
 
     # Search
     documents = SearchQuerySet().autocomplete(content=search_keyword).highlight().models(Hotel_Review)
+    try:
+        advanced_search = request.POST.__getitem__('advanced_search')
+    except KeyError:
+        advanced_search = 'false'
+    try:
+        hotel_name = request.POST.getlist('hotel_name')
+        if hotel_name:
+            documents = documents.filter(hotel_name__in=hotel_name)
+    except KeyError:
+        hotel_name = []
+    try:
+        hotel_star = request.POST.__getitem__('hotel_star')
+        hotel_star = float(hotel_star)
+        documents = documents.filter(hotel_star=hotel_star)
+    except KeyError:
+        hotel_star = ""
+    try:
+        hotel_rating = request.POST.__getitem__('hotel_rating')
+        documents = documents.filter(hotel_rating__gte=hotel_rating)
+    except KeyError:
+        hotel_rating = ""
+    try:
+        review_type = request.POST.getlist('review_type')
+        if review_type:
+            for type in review_type:
+                print type
+            documents = documents.filter(label__in=review_type)
+    except KeyError:
+        review_type = []
+    try:
+        review_rating = request.POST.__getitem__('review_rating')
+        documents = documents.filter(review_rating__gte=review_rating)
+    except KeyError:
+        review_rating = ""
+
+    # Get pages
+    try:
+        page = request.POST.__getitem__('page')
+        page = int(page)
+    except KeyError:
+        page = 1
+    if page <= 0:
+        page = 1
 
     # Paging
     complete_documents = documents
@@ -85,9 +121,20 @@ def search_documents(request):
     execution_time = end_time - start_time
     time = 'Executed in ' + str(execution_time.seconds) + '.' + str(execution_time.microseconds)+ ' second(s).'
 
+    # Prepare form for next query
+    search_documents_form = SearchDocumentsForm(initial={
+        'search_keyword': search_keyword,
+        'advanced_search': advanced_search,
+        'hotel_name': hotel_name,
+        'hotel_star': hotel_star,
+        'hotel_rating': hotel_rating,
+        'review_type': review_type,
+        'review_rating': review_rating,
+    })
+
     return render(request, 'result.html', {'search_keyword':search_keyword_original, 'documents': documents, 'page': page,
                                            'need_spell_correction': need_spell_correction,
                                            'search_keyword_spell_correct': search_keyword_spell_correct,
-                                           'time': time,
+                                           'advanced_search': advanced_search,
+                                           'time': time, 'form':search_documents_form,
                                            'has_previous_page': has_previous_page, 'has_next_page': has_next_page})
-
